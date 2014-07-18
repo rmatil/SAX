7bad68a68f27fad12b993eeeb696b8510eb3e632
Raphael Matile <raphael.matile@bluewin.ch>
2014-07-18 14:43:00 +0200

    Add a test for hasSubstring

--- test/SAX/SuffixTree/SuffixTreeTest.php
+++ test/SAX/SuffixTree/SuffixTreeTest.php
@@ -16,9 +16,10 @@ class SuffixTreeTest extends PHPUnit_Framework_TestCase {
         for ($i=1; $i < strlen( $this->stringToTest ) + 1; $i++) {
             for ($j=0; $j < strlen( $this->stringToTest ) - $i + 1; $j++) { 
                 $substring = substr($this->stringToTest, $j, $i);
-                $this->assertEquals(1, $this->suffixTree->hasSubstring($substring));
+                $this->assertEquals( 1, $this->suffixTree->hasSubstring( $substring ) );
             }
         }
+        $this->assertEquals( -1, $this->suffixTree->hasSubstring("aölskdj" ) );
     }
 
     public function testGetOccurence() {
@@ -27,6 +28,17 @@ class SuffixTreeTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(1, $this->suffixTree->getOccurence("pi"));
         $this->assertEquals(4, $this->suffixTree->getOccurence("i"));
         $this->assertEquals(2, $this->suffixTree->getOccurence("ss"));
+    }
+
+    public function testHasSubstring2() {
+        $string = "baedea";
+        $tree = new SuffixTree( $string );
+
+        $this->assertEquals(  1, $tree->hasSubstring( "ba" ) );
+        $this->assertEquals( -1, $tree->hasSubstring( "ee" ) );
+        $this->assertEquals( -1, $tree->hasSubstring( "baeb" ) );
+        $this->assertEquals( -1, $tree->hasSubstring( "aeb" ) );
+        $this->assertEquals( -1, $tree->hasSubstring( "aa" ) );
     }
 }
 
